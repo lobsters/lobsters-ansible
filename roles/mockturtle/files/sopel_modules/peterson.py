327c2c61adbfb0816e6454eea77efdaecee1bf44
Benjamin Buccianti <benjamin@buccianti.dev>
2019-12-02 18:18:47 -0300

    roles/mockturtle: add a limit of 5min to the peterson trigger

--- roles/mockturtle/files/sopel_modules/peterson.py
+++ roles/mockturtle/files/sopel_modules/peterson.py
@@ -5,6 +5,7 @@ import sopel.bot as bot
 import sopel
 
 @sopel.module.rule(r'.*[Pp]eterson.*')
+@sopel.module.rate(channel=300)
 def peterson(bot, trigger):
     """Link to FAQ if Peterson is mentioned"""
     bot.reply("The site has nothing to do with Jordan Peterson: https://lobste.rs/about#michaelbolton")
