31044dc554319120728a297203f012e1df43f647
Peter Bhat Harkins <peter@push.cx>
2024-02-06 12:10:48 -0600

    redirect www.lobsters.dev to lobsters.dev

--- roles/nginx/files/production/nginx/sites-available/lobste.rs
+++ roles/nginx/files/production/nginx/sites-available/lobste.rs
@@ -152,6 +152,27 @@ server {
   ssl_dhparam /etc/ssl/dhparams.pem;
   ssl_stapling on;
 }
+# redirect www.lobsters.dev -> lobsters.dev
+server {
+  listen 443 ssl http2 ;
+  listen [::]:443 ssl http2 ;
+  server_name www.lobsters.dev; # managed by Certbot
+
+  access_log /srv/lobste.rs/log/redirect.access.log main;
+  error_log /srv/lobste.rs/log/redirect.error.log;
+
+  rewrite ^/(.*)$ https://lobsters.dev/$1 permanent;
+  keepalive_timeout 0;
+
+  ssl_certificate /etc/letsencrypt/live/lobste.rs/fullchain.pem; # managed by Certbot
+  ssl_certificate_key /etc/letsencrypt/live/lobste.rs/privkey.pem; # managed by Certbot
+
+  ssl_protocols TLSv1.2;
+  ssl_ciphers ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS;
+  ssl_prefer_server_ciphers on;
+  ssl_dhparam /etc/ssl/dhparams.pem;
+  ssl_stapling on;
+}
 
 # lobsters.dev backup, serve an explanation page
 # to use lobsters.dev in prod, delete this section and add lobsters.dev to the first server block
